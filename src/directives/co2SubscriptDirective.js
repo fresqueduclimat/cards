9b3998b363063ea0e219a124dfceafacdedd6bfc
Léa Juskiewenski <lea.juskiewenski@fresqueduclimat.org>
2023-06-27 14:35:02 +0200

    update kh br fr and subscript directive

--- src/directives/co2SubscriptDirective.js
+++ src/directives/co2SubscriptDirective.js
@@ -1,7 +1,31 @@
 export default {
     beforeMount(el) {
-        const regex = /CO2/g;
-        const replacement = 'CO<sub>2</sub>';
+        var regex = /CO2/g;
+        var replacement = 'CO<sub>2</sub>';
+        el.innerHTML = el.innerHTML.replace(regex, replacement);
+
+        regex = /H2CO3/g;
+        replacement = 'H<sub>2</sub>CO<sub>3</sub>';
+        el.innerHTML = el.innerHTML.replace(regex, replacement);
+
+        regex = /HCO3/g;
+        replacement = 'HCO<sub>3</sub>';
+        el.innerHTML = el.innerHTML.replace(regex, replacement);
+
+        regex = /CH4/g;
+        replacement = 'CH<sub>4</sub>';
+        el.innerHTML = el.innerHTML.replace(regex, replacement);
+
+        regex = /N2O/g;
+        replacement = 'N<sub>2</sub>O';
+        el.innerHTML = el.innerHTML.replace(regex, replacement);
+
+        regex = /H2O/g;
+        replacement = 'H<sub>2</sub>O';
+        el.innerHTML = el.innerHTML.replace(regex, replacement);
+
+        regex = /O3/g;
+        replacement = 'O<sub>3</sub>O';
         el.innerHTML = el.innerHTML.replace(regex, replacement);
     },
 };
